f3ebfa61686619b3ac36b72bd1bd47d08e8cdd7a
AK <ak8893893@yahoo.com.tw>
2023-07-28 04:24:43 +0800

    follow the line and sing

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+music.play(music.string_playable("E E F G G F E D C C D E E D D - E E F G G F E D C C D E D C C - D D E C D E F E C D E F E D C D -", 240),
+    music.PlaybackMode.LOOPING_IN_BACKGROUND)
+
 def on_forever():
-    music.play(music.string_playable("E E F G G F E D C C D E E D D - E E F G G F E D C C D E D C C - D D E C D E F E C D E F E D C D -", 240),
-        music.PlaybackMode.UNTIL_DONE)
-    music.play(music.string_playable("- - - - - - - - ", 112),
-        music.PlaybackMode.UNTIL_DONE)
+    if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 0 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0:
+        maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 200)
+    if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0:
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 200)
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 10)
+    if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 0 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 1:
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 10)
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 200)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,17 @@
+music.play(music.stringPlayable("E E F G G F E D C C D E E D D - E E F G G F E D C C D E D C C - D D E C D E F E C D E F E D C D -", 240), music.PlaybackMode.LoopingInBackground)
 basic.forever(function on_forever() {
-    music.play(music.stringPlayable("E E F G G F E D C C D E E D D - E E F G G F E D C C D E D C C - D D E C D E F E C D E F E D C D -", 240), music.PlaybackMode.UntilDone)
-    music.play(music.stringPlayable("- - - - - - - - ", 112), music.PlaybackMode.UntilDone)
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 200)
+    }
+    
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 200)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 10)
+    }
+    
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 200)
+    }
+    
 })
