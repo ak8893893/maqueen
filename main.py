2294e6da43283c47b6250cf7349d79195aadbf01
AK <ak8893893@yahoo.com.tw>
2023-08-07 22:07:27 +0800

    manual-controller

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def on_forever():
-    music.play(music.string_playable("E E F G G F E D C C D E E D D - E E F G G F E D C C D E D C C - D D E C D E F E C D E F E D C D -", 240),
-        music.PlaybackMode.UNTIL_DONE)
-    music.play(music.string_playable("- - - - - - - - ", 112),
-        music.PlaybackMode.UNTIL_DONE)
-basic.forever(on_forever)
+
+def on_ir_callbackuser(message):
+    if message == 17:
+        maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 150)
+        maqueen.write_led(maqueen.LED.LED_LEFT, maqueen.LEDswitch.TURN_ON)
+        maqueen.write_led(maqueen.LED.LED_RIGHT, maqueen.LEDswitch.TURN_ON)
+    if message == 25:
+        maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CCW, 150)
+        maqueen.write_led(maqueen.LED.LED_LEFT, maqueen.LEDswitch.TURN_OFF)
+        maqueen.write_led(maqueen.LED.LED_RIGHT, maqueen.LEDswitch.TURN_OFF)
+        music.play(music.string_playable("C5 B A G F E D C ", 120),
+            music.PlaybackMode.UNTIL_DONE)
+    if message == 20:
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 150)
+        maqueen.write_led(maqueen.LED.LED_LEFT, maqueen.LEDswitch.TURN_ON)
+        maqueen.write_led(maqueen.LED.LED_RIGHT, maqueen.LEDswitch.TURN_OFF)
+    if message == 22:
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 150)
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        maqueen.write_led(maqueen.LED.LED_LEFT, maqueen.LEDswitch.TURN_OFF)
+        maqueen.write_led(maqueen.LED.LED_RIGHT, maqueen.LEDswitch.TURN_ON)
+    if message == 21:
+        maqueen.motor_stop(maqueen.Motors.ALL)
+        maqueen.write_led(maqueen.LED.LED_LEFT, maqueen.LEDswitch.TURN_OFF)
+        maqueen.write_led(maqueen.LED.LED_RIGHT, maqueen.LEDswitch.TURN_OFF)
+IR.IR_callbackUser(on_ir_callbackuser)
--- main.ts
+++ main.ts
@@ -1,4 +1,30 @@
-basic.forever(function on_forever() {
-    music.play(music.stringPlayable("E E F G G F E D C C D E E D D - E E F G G F E D C C D E D C C - D D E C D E F E C D E F E D C D -", 240), music.PlaybackMode.UntilDone)
-    music.play(music.stringPlayable("- - - - - - - - ", 112), music.PlaybackMode.UntilDone)
+IR.IR_callbackUser(function (message) {
+    if (message == 17) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    }
+    if (message == 25) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 150)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
+    }
+    if (message == 20) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 150)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    }
+    if (message == 22) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 150)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    }
+    if (message == 21) {
+        maqueen.motorStop(maqueen.Motors.All)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    }
 })
